33917b6785eac56f63868d5b480e337e13289022
John <jjh5166@gmail.com>
2020-11-28 14:24:44 -0500

    make model

---
+++ app/Models/Pizza.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+// $php artisan make:model Pizza
+class Pizza extends Model
+{
+    use HasFactory;
+}
