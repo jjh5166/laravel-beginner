51441b2a103a8742c2d2653ff3d964762d6ca8ff
John <jjh5166@gmail.com>
2020-11-29 21:10:11 -0500

    create form/welcome link

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title">Create Pizza</div>
-    </div>
+<div class="wrapper create-pizza">
+<h1>Create A New Pizza</h1>
+<form action="/pizzas" method="POST">
+    <label for="name">Your Name</label>
+    <input type="text" id="name" name="name">
+    <label for="type">Choose Pizza Type</label>
+    <select name="type" id="type">
+        <option value="margarita">margarita</option>
+        <option value="hawaiian">hawaiian</option>
+        <option value="volcano">volcano</option>
+        <option value="veg supreme">veg supreme</option>
+    </select>
+    <label for="base">Choose base type</label>
+    <select name="base" id="base">
+        <option value="cheesy crust">cheesy crust</option>
+        <option value="garlic crust">garlic crust</option>
+        <option value="thin & crispy">thin & crispy</option>
+        <option value="Thick">Thick</option>
+    </select>
+    <input type="submit" value="Order Pizza">
+</form>
 </div>
 @endsection
